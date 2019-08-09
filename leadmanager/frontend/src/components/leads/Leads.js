5036076e6945b1ad2f11d4124afbe95d2ba7e97d
Gilwell Muhati <gilwell.muhati@tezzasolutions.com>
2019-08-09 09:10:58 +0300

    Added State and Reducers, Received Leads from the backend

--- leadmanager/frontend/src/components/leads/Leads.js
+++ leadmanager/frontend/src/components/leads/Leads.js
@@ -1,13 +1,62 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react';
+import { connect } from 'react-redux';
+import { PropTypes } from 'prop-types';
+import { getLeads } from '../../actions/leads';
+
 
 export class Leads extends Component {
+    static propTypes = {
+        leads: PropTypes.array.isRequired
+    }
+
+    componentDidMount() {
+        this.props.getLeads();
+    }    
+
+
     render() {
         return (
-            <div>
-                <h1>Leads</h1>
-            </div>
+            <Fragment>
+                <h1>Leads List</h1>
+                <table className="table table-stripped">
+                    <thead>
+                        <tr>
+                            <th>
+                                ID
+                            </th>
+                            <th>
+                                NAME
+                            </th>
+                            <th>
+                                EMAIL
+                            </th>
+                            <th>
+                                MESSAGE
+                            </th>
+                            <th>
+                                
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        { this.props.leads.map(lead => (
+                            <tr key={lead.id}>
+                                <td>{lead.name}</td>
+                                <td>{lead.email}</td>
+                                <td>{lead.message}</td>
+                                <td><button className="btn btn-danger btn-sm">DELETE</button></td>
+                             
+                            </tr>
+                        )) }
+                    </tbody>
+                </table>
+            </Fragment>
         )
     }
 }
 
-export default Leads
+const mapStateToProps = state => ({
+    leads: state.leads.leads
+});
+
+export default connect(mapStateToProps, { getLeads })(Leads);
